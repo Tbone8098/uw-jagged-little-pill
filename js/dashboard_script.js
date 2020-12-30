d462f0148101ee195827594253a7e4fe6583266c
Tyler <thibault.tyler@gmail.com>
2020-12-30 14:06:40 -0800

    can create playlist off of time given

--- js/dashboard_script.js
+++ js/dashboard_script.js
@@ -1,43 +1,55 @@
 $(document).ready(async function () {
     // get mood from localstorage
-    // call sheets API and get all songs for mood.
+    var name = "Tyler";
     var mood = "happy";
+    var timeGiven = 48;
+    var timeGivenInMilli = timeGiven * 60 * 1000;
+    console.log(timeGivenInMilli);
+
+    // call sheets API and get all songs for mood.
     const allSongs = await getSongsAPI(mood);
     console.log(allSongs);
+
     // create new var: totalPlaylistDuration (int)
     var totalPlaylistDuration = 0;
+
     // create new var: Playlist (dict)
     var playList = [];
+
     // loop (do while) through length of returned array
     var songsNumAlreadyChosen = [];
     var i = 0;
-    do {
+    while (totalPlaylistDuration < timeGivenInMilli) {
+        // break if it has added all songs to the playlist
+        if (i >= allSongs[mood].length) {
+            // TODO add error message saying all songs are added to playlist
+            break;
+        }
         // pick one at random
         var ranNum = Math.floor(Math.random() * allSongs[mood].length);
         if (songsNumAlreadyChosen.indexOf(ranNum) == -1) {
             var currentSong = allSongs[mood][ranNum];
             songsNumAlreadyChosen.push(ranNum);
+
             // add to playlist
             playList.push(currentSong.youtubeId);
-            // add duration from api to totalPlaylistDuration
+
+            // get the duration in milliseconds
             var durationArray = currentSong.duration.split(":");
-            var hours = parseInt(durationArray[0]);
-            var minutes = parseInt(durationArray[1]);
-            var seconds = parseInt(durationArray[2]);
+            var hoursInMilli = parseInt(durationArray[0]) * 60 * 60 * 1000;
+            var minutesInMilli = parseInt(durationArray[1]) * 60 * 1000;
+            var secondsInMilli = parseInt(durationArray[2]) * 1000;
+            var duration = hoursInMilli + minutesInMilli + secondsInMilli;
 
-            // var duration = parseFloat(currentSong.duration);
-            // totalPlaylistDuration += duration;
-
-            console.log(
-                `this song is ${hours} hours ${minutes} minutes and ${seconds} seconds long`
-            );
+            // add duration from api to totalPlaylistDuration
+            totalPlaylistDuration += duration;
+            i++;
         }
-        i++;
-    } while (i < allSongs[mood].length);
+    }
+    // while (i < allSongs[mood].length);
 
-    // console.log(totalPlaylistDuration);
+    console.log(totalPlaylistDuration);
     console.log(playList);
-    // when totalPlaylistDuration > walkTime from localStorage break loop.
 });
 
 // functions **************************
