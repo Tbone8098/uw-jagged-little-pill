6f508e06d7f5b2595c773a027f173a4e198d17f4
Tyler <thibault.tyler@gmail.com>
2020-12-30 16:14:08 -0800

    wip

--- js/dashboard_script.js
+++ js/dashboard_script.js
@@ -2,7 +2,8 @@ $(document).ready(async function () {
     // get mood from localstorage
     var name = "Tyler";
     var mood = "happy";
-    var timeGiven = 15;
+    var timeGiven = 45;
+
     var timeGivenInMilli = timeGiven * 60 * 1000;
     console.log(timeGivenInMilli);
 
@@ -60,14 +61,3 @@ async function getSongsAPI(mood) {
 
     return allSongs;
 }
-
-let map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
-
-initMap();
--- js/index_script.js
+++ js/index_script.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     submitBtn.addEventListener("click", function (event) {
         event.preventDefault();
         // check to see if info on form exists
-
+        console.log("click", click);
         // if true then store to localstorage
         // and go to dashboard
-        location.href = "dashboard.html";
+        // location.href = "dashboard.html";
 
         // else populate and display error message
     });
