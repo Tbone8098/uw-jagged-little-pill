91defa09243ed4464544887cb9474280f98c33ee
Tyler <thibault.tyler@gmail.com>
2021-01-03 21:21:46 -0800

    idk

--- js/dashboard_script.js
+++ js/dashboard_script.js
@@ -101,8 +101,11 @@ function nextSong() {
 // ********************************************************
 // ******************************************************** YouTube
 // ********************************************************
-var tag;
-var firstScriptTag;
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function setupApi() {
     // 2. This code loads the IFrame Player API code asynchronously.
     tag = document.createElement("script");
